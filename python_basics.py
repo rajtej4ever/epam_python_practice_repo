34a428c8941b0d37b0b23d2e8efd79644203ef4d
rajtej4ever <108463697+rajtej4ever@users.noreply.github.com>
2024-02-27 16:39:34 +0530

    session 1

---
+++ python_basics.py
@@ -0,0 +1,61 @@
+#boolean variables
+
+"""a=True
+b= False
+
+print(a)
+print(b)
+
+
+# int
+a=1
+print(a)
+
+# float
+print(1.2-1.0)
+
+
+a='1\n2\n3'
+
+print(b)
+
+
+# list
+
+b=[1,2,3,'String',True]
+b[3]='something'
+
+print(b)
+
+
+# tuple immutable mixed datatype
+
+a=(1,2,3.0,'string',True)
+
+
+# dictionary
+a={'key':3,4:False}
+#print(a.get('key'))
+for k,v in a.items():
+    print(k)
+    print(v)
+
+
+b=range(10,-5,-3)
+for x in b:
+    print(x,end=" ")
+
+
+
+lst1=[1,2,3,4,5,6]
+for i in lst1:
+    print(i)
+
+
+"""
+
+# while
+i=2
+while i<5:
+    print(i)
+    i+=1
